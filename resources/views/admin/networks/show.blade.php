c13550e04fd907580f026835579caccbaf60079a
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2025-12-10 19:04:58 +0330

    Fixed a minor issue

--- resources/views/admin/networks/show.blade.php
+++ resources/views/admin/networks/show.blade.php
@@ -58,7 +58,7 @@
 </div>
 
 <!-- Main Content Grid -->
-<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
     <!-- Main Content -->
     <div class="lg:col-span-2 space-y-6">
         <!-- Network Details Card -->
