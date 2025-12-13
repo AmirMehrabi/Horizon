0ed074d763dab3d2841e8008ea5dd9dc0f05392d
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2025-12-13 16:34:17 +0330

    Fixed a minor issue

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -92,7 +92,7 @@
     </div>
 </div>
 
-<div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
     <!-- Main Content -->
     <div class="lg:col-span-2 space-y-6">
         <!-- Active VPS -->
